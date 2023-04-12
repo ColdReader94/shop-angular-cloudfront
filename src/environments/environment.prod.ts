39c64d333e609627afc67b3bc14c7050e27afa67
Vadzim Sidarau <Vadzim_Sidarau@epam.com>
2023-04-12 19:40:02 +0300

    feat: add api endpoints for import

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://qs81cs1583.execute-api.us-east-1.amazonaws.com/',
     bff: 'https://4e72io0u32.execute-api.us-east-1.amazonaws.com',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
